a7e5392cd38de2544c07eb1665ae701564fd0998
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-06-27 15:31:42 -0400

    added prompt for writing readme file

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 function writeToFile({title, description, tableOfContents, installation, usage, license, contributing, test, questions}) {
     `# ${title}
 
@@ -26,3 +29,53 @@ function writeToFile({title, description, tableOfContents, installation, usage, 
     ${questions}
     `;
 }
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Please provide a brief description of your project.',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Please provide a table of content for your project',
+            name: 'tableOfContents',
+        },
+        {
+            type: 'installation',
+            message: 'Please provide a description on the installation process for this project.',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Please describe the usage of this project',
+            name: 'usage',
+        },
+        {
+            type: 'list',
+            message: 'Please choose a license for project.',
+            name: 'license',
+            choices: ["MIT", "LSC", "Apache License 2.0"],
+        },
+        {
+            type: 'input',
+            message: 'Please describe any contributions towards the production of this project',
+            name: 'contributions',
+        },
+        {
+            type: 'input',
+            message: 'please describe the testing process for this project.',
+            name: 'testing',
+        },
+        {
+            type: 'input',
+            message: 'Pleasee describe how users are able to submit any questions regarding the project.',
+            name: 'questions',
+        },
+    ])
